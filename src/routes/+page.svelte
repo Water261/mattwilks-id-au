556367028bfc1e6aacd3b355c53f389ee0ccfb44
Matthew Wilks <water261@outlook.com.au>
2023-03-07 09:45:03 +1000

    Migrate to single file to avoid bulma reimport

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,84 @@
-<section class="hero is-fullheight hero-background">
-	<div class="hero-body is-flex is-justify-content-center is-align-items-start mt-6">
-		<div>
-			<h1 class="is-size-1 title">Matthew Wilks</h1>
-			<h3 class="is-size-3 subtitle">A hobby developer</h3>
+<script lang="ts">
+	import { writable } from 'svelte/store';
+
+	const navbarState = writable(false);
+</script>
+
+<svelte:head>
+	<script src="https://kit.fontawesome.com/b08b27e57b.js" crossorigin="anonymous"></script>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<div id="wrapper" class="has-navbar-fixed-top">
+	<nav class="navbar is-fixed-top">
+		<div class="navbar-brand">
+			<!-- svelte-ignore a11y-invalid-attribute -->
+			<a href="#" class="navbar-item">
+				<h1>Matthew Wilks</h1>
+			</a>
+			<button
+				class={`navbar-burger ${$navbarState ? 'is-active' : ''}`}
+				aria-label="menu"
+				aria-expanded="false"
+				data-target="navbarMenu"
+				on:click={() => ($navbarState = !$navbarState)}
+			>
+				<span aria-hidden="true" />
+				<span aria-hidden="true" />
+				<span aria-hidden="true" />
+			</button>
 		</div>
-	</div>
-</section>
-<p class="mt-1">Photo by Eberhard Grossgasteiger</p>
-<section id="about" class="section is-small">
-	<h2 class="title is-size-2">About Me</h2>
-	<p>
-		Hi there! I&apos;m Matthew, a hobby developer who&apos;s interested in Rust, C# &amp;
-		Typescript.
-	</p>
-</section>
-<section id="projects" class="section is-small">
-	<h2 class="title is-size-2">Projects</h2>
-</section>
-<section id="contact" class="section is-small">
-	<h2 class="title is-size-2">Contact Me</h2>
-</section>
+		<div id="navbarMenu" class={`navbar-menu ${$navbarState ? 'is-active' : ''}`}>
+			<div class="navbar-end">
+				<a href="#about" class="navbar-item">About Me</a>
+				<a href="#projects" class="navbar-item">My Projects</a>
+				<a href="#contact" class="navbar-item">Contact Me</a>
+			</div>
+		</div>
+	</nav>
+	<main class="has-text-centered">
+		<section class="hero is-fullheight hero-background">
+			<div class="hero-body is-flex is-justify-content-center is-align-items-start mt-6">
+				<div>
+					<h1 class="is-size-1 title">Matthew Wilks</h1>
+					<h3 class="is-size-3 subtitle">A hobby developer</h3>
+				</div>
+			</div>
+		</section>
+		<p class="mt-1">Photo by Eberhard Grossgasteiger</p>
+		<section id="about" class="section is-small">
+			<h2 class="title is-size-2">About Me</h2>
+			<p>
+				Hi there! I&apos;m Matthew, a hobby developer who&apos;s interested in Rust, C# &amp;
+				Typescript.
+			</p>
+		</section>
+		<section id="projects" class="section is-small">
+			<h2 class="title is-size-2">Projects</h2>
+		</section>
+		<section id="contact" class="section is-small">
+			<h2 class="title is-size-2">Contact Me</h2>
+		</section>
+	</main>
+	<footer class="footer has-text-centered">
+		<p>
+			<span>&copy; 2023 - Matthew Wilks. Licensed under </span>
+			<a href="https://creativecommons.org/licenses/by-nd/4.0/">CC BY-ND 4.0</a>
+		</p>
+	</footer>
+</div>
 
 <style lang="scss">
 	@import '../../node_modules/bulma/bulma.sass';
+
+	div#wrapper {
+		font-family: 'Roboto', sans-serif;
+	}
 
 	.hero-background {
 		background-image: url('/img/hero-background.jpg');
