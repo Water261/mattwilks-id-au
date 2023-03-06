e28137d43edfdf9755c239b69a48e171c2204e4c
Matthew Wilks <water261@outlook.com.au>
2023-03-06 17:58:00 +1000

    Fix styling issues

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,8 +11,8 @@
 	<link
 		href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
 		rel="stylesheet"
-	/></svelte:head
->
+	/>
+</svelte:head>
 
 <div id="wrapper">
 	<nav class="navbar">
@@ -40,7 +40,7 @@
 			</div>
 		</div>
 	</nav>
-	<main>
+	<main class="has-text-centered">
 		<slot />
 	</main>
 	<footer class="footer has-text-centered">
