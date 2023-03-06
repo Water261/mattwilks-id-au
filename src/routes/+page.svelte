ab6a51620f659c26445c289ebbc3c02ce1b1aabf
Matthew Wilks <water261@outlook.com.au>
2023-03-06 17:58:14 +1000

    Add home page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<section class="hero is-fullheight-with-navbar hero-background">
+	<div class="hero-body is-flex is-justify-content-center is-align-items-start mt-6">
+		<div>
+			<h1 class="is-size-1 title">Matthew Wilks</h1>
+			<h3 class="is-size-3 subtitle">A hobby developer</h3>
+		</div>
+	</div>
+</section>
+<p>Photo by Eberhard Grossgasteiger</p>
+<section id="about" class="section is-small">
+	<h2 class="title is-size-2">About Me</h2>
+	<p>
+		Hi there! I&apos;m Matthew, a hobby developer who&apos;s interested in Rust, C# &amp;
+		Typescript.
+	</p>
+</section>
+<section id="projects" class="section is-small">
+	<h2 class="title is-size-2">Projects</h2>
+</section>
+<section id="contact" class="section is-small">
+	<h2 class="title is-size-2">Contact Me</h2>
+</section>
+
+<style lang="scss">
+	@import '../../node_modules/bulma/bulma.sass';
+
+	.hero-background {
+		background-image: url('/img/hero-background.jpg');
+		background-size: cover;
+		background-position: center;
+	}
+</style>
