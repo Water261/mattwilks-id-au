848937dd7d90746589e2c50fecc3206805fdd3b4
Matthew Wilks <water261@outlook.com.au>
2023-03-06 17:24:36 +1000

    Add fontawesome

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,9 @@
 <style lang="scss">
 	@import "../../node_modules/bulma/bulma.sass";
 </style>
+
+<svelte:head>
+	<script src="https://kit.fontawesome.com/b08b27e57b.js" crossorigin="anonymous"></script>
+</svelte:head>
+
+<slot></slot>
