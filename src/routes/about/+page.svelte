a27190598edfbebb5871dd5789a3e6add2df7bf4
Matthew Wilks <water261@outlook.com.au>
2023-03-08 12:26:44 +1000

    Add content to about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,4 +2,19 @@
 	<title>About Me - Matthew Wilks</title>
 </svelte:head>
 
-<h1 class="text-4xl">About Me</h1>
+<div class="hero h-[87vh]">
+	<div class="hero-content flex-col lg:flex-row">
+		<img src="https://placehold.co/400x600?font=roboto" alt="" class="max-w-sm rounded-lg shadow-lg" />
+		<div class="lg:w-[40rem]">
+			<h1 class="text-4xl font-bold">About Me</h1>
+			<p class="p-6">
+				Hi! I'm Matthew, a hobby developer who loves to work with C#, Rust & Typescript.
+				Some of my projects include a Tetris clone using Phaser, this portfolio and a Security Support Provider & Authentication Package for kanidm.
+			</p>
+			<a href="/projects" class="btn btn-primary gap-2">
+				<p>My Projects</p>
+				<i class="fa-solid fa-arrow-right fa-xl"></i>
+			</a>
+		</div>
+	</div>
+</div>
