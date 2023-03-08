7e28a0b74038df69fe0bfb0ed89ace99cfe175d2
Matthew Wilks <water261@outlook.com.au>
2023-03-08 11:42:47 +1000

    Use emerald theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,20 +6,6 @@ module.exports = {
   },
   plugins: [require('daisyui')],
   daisyui: {
-    themes: [
-      {
-        webtheme: {
-          "primary": "#5eead4",
-          "secondary": "#38bdf8",
-          "accent": "#1FB2A5",
-          "neutral": "#191D24",
-          "base-100": "#2A303C",
-          "info": "#3ABFF8",
-          "success": "#36D399",
-          "warning": "#FBBD23",
-          "error": "#F87272",
-        },
-      },
-    ],
-  },
+    themes: ["emerald"]
+  }
 }
