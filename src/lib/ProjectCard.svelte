9baa97e599a6a1fa5a2d144f20b104317c7d292e
Matthew Wilks <water261@outlook.com.au>
2023-03-09 10:13:26 +1000

    Add project cards and some projects

---
+++ src/lib/ProjectCard.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	export let projectName: string;
+	export let projectDesc: string;
+	export let projectImgPath: string;
+	export let projectLink: string;
+	let projectLinkNoProto = projectLink.replace('https://', '');
+</script>
+
+<div class="m-auto w-2/5 shadow-xl border-2 rounded-lg h-72 text-center flex mt-4">
+	<img src={projectImgPath} alt={`${projectName} Logo`} class="h-full aspect-square rounded-l-lg" />
+	<div class="p-4 flex flex-col gap-4">
+		<div class="h-1/4">
+			<h1 class="text-3xl text-bold">{projectName}</h1>
+			<a href={projectLink} class="link-info">
+				{projectLinkNoProto}
+				<i class="fa-solid fa-arrow-up-right-from-square fa-2xs relative bottom-1" />
+			</a>
+		</div>
+		<p class="text-md h-2/4">
+			{projectDesc}
+		</p>
+		<div class="h-1/4">
+			<a href={projectLink} target="_blank" rel="noreferrer" class="btn btn-primary"
+				>Visit Project</a
+			>
+		</div>	
+	</div>
+</div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,3 +1,8 @@
+<script>
+	// @ts-ignore
+	import ProjectCard from '$lib/ProjectCard.svelte';
+</script>
+
 <svelte:head>
 	<title>My Projects - Matthew Wilks</title>
 </svelte:head>
@@ -6,3 +11,18 @@
 	<h1 class="text-4xl font-bold">My Projects</h1>
 	<p class="py-6">Below you will find my projects and others that I have contributed to.</p>
 </div>
+
+<ProjectCard
+	projectName="kanidm"
+	projectDesc="Kanidm is a simple and secure identity management platform, which provides services to allow
+		other systems and application to authenticate against. The project aims for the highest levels
+		of reliability, security and ease of use."
+	projectImgPath="/img/kanidm-logo.png"
+	projectLink="https://github.com/kanidm/kanidm"
+/>
+<ProjectCard
+	projectName="tetris-clone"
+	projectDesc="tetris-clone is my remake of tetris using the Phaser game framework."
+	projectImgPath="/img/phaser-logo.png"
+	projectLink="https://github.com/Water261/tetris-clone"
+/>
