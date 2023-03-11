fa77c661cdd3790ba94c9293a9c26210f6e9d76f
Matthew Wilks <water261@outlook.com.au>
2023-03-11 14:43:07 +1000

    Make ProjectCard resize nicer

--- src/lib/ProjectCard.svelte
+++ src/lib/ProjectCard.svelte
@@ -1,28 +1,62 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
+	import { writable } from 'svelte/store';
+
 	export let projectName: string;
 	export let projectDesc: string;
 	export let projectImgPath: string;
 	export let projectLink: string;
 	let projectLinkNoProto = projectLink.replace('https://', '');
+
+	const windowSize = writable(browser && window.innerWidth);
+
+	function onWindowResize() {
+		windowSize.set(browser && window.innerWidth);
+	}
 </script>
 
-<div class="m-auto w-2/5 shadow-xl border-2 rounded-lg h-72 text-center flex mt-4">
-	<img src={projectImgPath} alt={`${projectName} Logo`} class="h-full aspect-square rounded-l-lg" />
-	<div class="p-4 flex flex-col gap-4">
-		<div class="h-1/4">
-			<h1 class="text-3xl text-bold">{projectName}</h1>
+<svelte:window on:resize={onWindowResize} />
+
+{#if $windowSize >= 1024}
+	<div class="m-auto max-w-3xl shadow-xl border-2 rounded-lg h-72 text-center flex mt-4">
+		<img
+			src={projectImgPath}
+			alt={`${projectName} Logo`}
+			class="h-full aspect-square rounded-l-lg"
+		/>
+		<div class="p-4 flex flex-col gap-4">
+			<div class="h-1/4">
+				<h1 class="text-3xl text-bold">{projectName}</h1>
+				<a href={projectLink} target="_blank" rel="noreferrer" class="link-info">
+					{projectLinkNoProto}
+					<i class="fa-solid fa-arrow-up-right-from-square fa-2xs relative bottom-1" />
+				</a>
+			</div>
+			<p class="text-md h-2/4">
+				{projectDesc}
+			</p>
+			<div class="h-1/4">
+				<a href={projectLink} target="_blank" rel="noreferrer" class="btn btn-primary">
+					Visit Project
+				</a>
+			</div>
+		</div>
+	</div>
+{:else}
+	<div class="card w-96 bg-base-100 shadow-xl my-4 m-auto">
+		<figure><img src={projectImgPath} alt={`${projectName} Logo`} /></figure>
+		<div class="card-body">
+			<h2 class="card-title justify-center">{projectName}</h2>
 			<a href={projectLink} target="_blank" rel="noreferrer" class="link-info">
 				{projectLinkNoProto}
 				<i class="fa-solid fa-arrow-up-right-from-square fa-2xs relative bottom-1" />
 			</a>
-		</div>
-		<p class="text-md h-2/4">
-			{projectDesc}
-		</p>
-		<div class="h-1/4">
-			<a href={projectLink} target="_blank" rel="noreferrer" class="btn btn-primary">
-				Visit Project
-			</a>
+			<p>{projectDesc}</p>
+			<div class="card-actions justify-center">
+				<a href={projectLink} target="_blank" rel="noreferrer" class="btn btn-primary">
+					Visit Project
+				</a>
+			</div>
 		</div>
 	</div>
-</div>
+{/if}
